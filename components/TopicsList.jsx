638b7c74fa58cf323f1eaf02ec32cb5b6ae92a59
Faaz Laeeq <faazlaeeq@gmail.com>
2024-06-06 20:57:42 +0500

    getTopics fixed

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -4,7 +4,7 @@ import { HiPencilAlt } from "react-icons/hi";
 
 const getTopics = async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/topics");
+    const res = await fetch("{NEXT_PUBLIC_ROOT_URL}api/topics");
 
     if (!res.ok) {
       throw new Error("Failed to fetch topics");
