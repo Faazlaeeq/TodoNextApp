f5c20e345292614c2867b56ad9d57fe5d5d885ea
Faaz Laeeq <faazlaeeq@gmail.com>
2024-06-06 20:38:35 +0500

    getTopics fixed

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -4,43 +4,47 @@ import { HiPencilAlt } from "react-icons/hi";
 
 const getTopics = async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/topics", {
-      cache: "no-store",
-    });
+    const res = await fetch("http://localhost:3000/api/topics");
 
     if (!res.ok) {
       throw new Error("Failed to fetch topics");
     }
 
-    return res.json();
+    return res.json(); // Parse response body as JSON
   } catch (error) {
     console.log("Error loading topics: ", error);
   }
 };
 
 export default async function TopicsList() {
-  const { topics } = await getTopics();
+  try {
+    const myFetch = await getTopics();
+    console.log(myFetch["topics"]);
 
-  return (
-    <>
-      {topics.map((t) => (
-        <div
-          key={t._id}
-          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
-        >
-          <div>
-            <h2 className="font-bold text-2xl">{t.title}</h2>
-            <div>{t.description}</div>
-          </div>
+    return (
+      <>
+        {myFetch["topics"].map((t) => (
+          <div
+            key={t._id}
+            className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
+          >
+            <div>
+              <h2 className="font-bold text-2xl">{t.title}</h2>
+              <div>{t.description}</div>
+            </div>
 
-          <div className="flex gap-2">
-            <RemoveBtn id={t._id} />
-            <Link href={`/editTopic/${t._id}`}>
-              <HiPencilAlt size={24} />
-            </Link>
+            <div className="flex gap-2">
+              <RemoveBtn id={t._id} />
+              <Link href={`/editTopic/${t._id}`}>
+                <HiPencilAlt size={24} />
+              </Link>
+            </div>
           </div>
-        </div>
-      ))}
-    </>
-  );
+        ))}
+      </>
+    );
+  } catch (error) {
+    console.log("Error rendering topics: ", error);
+    return null; // Return null or some error component to handle errors
+  }
 }
